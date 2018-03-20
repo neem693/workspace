02ebdf705728eddfee6cc271980b9eec16b5bd59
neem693 <neem693@gmail.com>
2018-03-20 23:41:04 +0900

    update_home_practice_3_20

---
+++ studyjava/src/practice017/TestArray03.java
@@ -0,0 +1,15 @@
+package practice017;
+
+public class TestArray03 {
+
+	public static void printArray(int [] array) {
+		for(int i=0;i<array.length;i++) {
+			System.out.println(array[i] + " ");
+		}
+	}
+	
+	public static void main(String[] args) {
+		printArray(new int[] {3,1,2,6,4,2});
+	}
+
+}
