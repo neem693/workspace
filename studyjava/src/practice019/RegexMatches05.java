a405f15e5f5224d111536229d4801778fe57fc8e
Oratree <neem693@gmail.com>
2018-03-28 18:28:28 +0900

    3_28_update

--- studyjava/src/practice019/RegexMatches05.java
+++ studyjava/src/practice019/RegexMatches05.java
@@ -1,30 +1,24 @@
 package practice019;
 
-import java.util.*;
-import java.util.regex.*;
-  
-class RegexMatches05
-{
-  static Map props = new HashMap();
-  static
-  {
-    props.put("key1", "fox");
-    props.put("key2", "dog");
-  }
-  
-  public static void main(String[] args)
-  {
-    String input = "The quick brown ${key1} jumps over the lazy ${key2}.";
-  System.out.println(input);
-    Pattern p = Pattern.compile("\\$\\{([^}]+)\\}");
-    Matcher m = p.matcher(input);
-    StringBuffer sb = new StringBuffer();
-    while (m.find())
-    {
-      m.appendReplacement(sb, "-");
-      sb.append(props.get(m.group(1)));
-    }
-    m.appendTail(sb);
-    System.out.println(sb.toString());
-  }
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexMatches05 {
+
+   private static String REGEX = "a*b";
+   private static String INPUT = "aabfooaabfooabfoob";
+   private static String REPLACE = "-";
+   public static void main(String[] args) {
+
+      Pattern p = Pattern.compile(REGEX);
+      
+      // get a matcher object
+      Matcher m = p.matcher(INPUT);
+      StringBuffer sb = new StringBuffer();
+      while(m.find()) {
+         m.appendReplacement(sb, REPLACE);
+      }
+      m.appendTail(sb);
+      System.out.println(sb.toString());
+   }
 }
