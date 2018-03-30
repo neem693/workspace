0bcf1e98fc57f432c08667bf08c77b5b77198b27
Oratree <neem693@gmail.com>
2018-03-30 18:26:52 +0900

    3_30_update

---
+++ studyjava/src/practice020/Example01.java
@@ -0,0 +1,21 @@
+package practice020;
+
+public class Example01 {
+
+	public static void main(String[] args) {
+		int a = 11;
+		int b = 6;
+		int c = minFunction(a,b);
+		System.out.println("Minium Value = " + c);
+	}
+	public static int minFunction(int n1,int n2) {
+		int min;
+		if(n1>n2)
+			min = n2;
+		else
+			min = n1;
+		
+		return min;
+	}
+
+}
---
+++ studyjava/src/practice020/Example02.java
@@ -0,0 +1,18 @@
+package practice020;
+
+public class Example02 {
+
+	public static void main(String[] args) {
+		methodRankPoints(255.7);
+
+	}
+	public static void methodRankPoints(double points) {
+		if(points >= 202.5)
+			System.out.println("Rank:A1");
+		else if(points>=122.4)
+			System.out.println("Rank:A2");
+		else
+			System.out.println("Rank:A3");
+	}
+
+}
