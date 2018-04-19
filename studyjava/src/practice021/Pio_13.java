c7d4ed17b0693d91df9066f63f8fa5f875743319
Oratree <neem693@gmail.com>
2018-04-19 20:57:04 +0900

    04_19

---
+++ studyjava/src/practice021/Pio_13.java
@@ -0,0 +1,31 @@
+package practice021;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Pio_13 {
+
+	public static void main(String[] args) throws IOException{
+		File file = new File("Hello1.txt");
+		
+		file.createNewFile();
+		
+		FileWriter writer = new FileWriter(file);
+		
+		writer.write("This\n is\n an\n example\n");
+		writer.flush();
+		writer.close();
+		
+		FileReader fr = new FileReader(file);
+		char [] a =new char[50];
+		fr.read(a);
+		
+		for(char c : a)
+			System.out.print(c);
+		fr.close();
+
+	}
+
+}
---
+++ studyjava/src/practice022/Ex_01.java
@@ -0,0 +1,14 @@
+package practice022;
+
+import java.io.File;
+import java.io.FileReader;
+
+public class Ex_01 {
+
+	public static void main(String[] args) {
+		File file = new File("file.txt");
+		FileReader fr = new FileReader(file);
+
+	}
+
+}
