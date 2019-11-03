dcc83028396ea96ecab9713df3dcb140fb6ee717
neem693 <37389515+neem693@users.noreply.github.com>
2019-11-03 23:48:39 +0900

    update

---
+++ 필요한 튜토 정리/튜토.mm
@@ -0,0 +1,38 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1572720769531" ID="ID_279601719" MODIFIED="1572720815934" TEXT="&#xc6cd;&#xc2a4;&#xd398;&#xc774;&#xc2a4;">
+<node CREATED="1572720817682" ID="ID_1810967699" MODIFIED="1572720819780" POSITION="right" TEXT="html"/>
+<node CREATED="1572720819968" ID="ID_4027568" MODIFIED="1572720820381" POSITION="right" TEXT="js">
+<node CREATED="1572728054764" ID="ID_1084357392" MODIFIED="1572728055906" TEXT="jquery">
+<node CREATED="1572728056778" ID="ID_1486548757" MODIFIED="1572728062379" TEXT="offset&#xacfc; scroll&#xc5d0; &#xb300;&#xd55c; &#xd568;&#xc218;&#xb4e4;">
+<node CREATED="1572728062793" ID="ID_848771602" LINK="https://www.w3schools.com/jquery/jquery_ref_html.asp" MODIFIED="1572728062793" TEXT="https://www.w3schools.com/jquery/jquery_ref_html.asp"/>
+<node CREATED="1572728177696" ID="ID_838933367" MODIFIED="1572728181825" TEXT="offset">
+<node CREATED="1572728501738" LINK="https://www.w3schools.com/jquery/css_offset.asp" MODIFIED="1572728501738" TEXT="https://www.w3schools.com/jquery/css_offset.asp"/>
+</node>
+<node CREATED="1572728192401" ID="ID_224200263" MODIFIED="1572728200650" TEXT="scrollTOp">
+<node CREATED="1572728477167" LINK="https://www.w3schools.com/jsref/prop_element_scrolltop.asp" MODIFIED="1572728477167" TEXT="https://www.w3schools.com/jsref/prop_element_scrolltop.asp"/>
+</node>
+<node CREATED="1572728200856" ID="ID_1030437870" MODIFIED="1572728204269" TEXT="width">
+<node CREATED="1572728238824" LINK="https://www.w3schools.com/jquery/css_width.asp" MODIFIED="1572728238824" TEXT="https://www.w3schools.com/jquery/css_width.asp"/>
+</node>
+<node CREATED="1572728204763" ID="ID_1824472481" MODIFIED="1572728207219" TEXT="height">
+<node CREATED="1572728220647" LINK="https://www.w3schools.com/jquery/css_height.asp" MODIFIED="1572728220647" TEXT="https://www.w3schools.com/jquery/css_height.asp"/>
+</node>
+<node CREATED="1572728207532" ID="ID_1280395477" MODIFIED="1572728212537" TEXT="outerheight">
+<node CREATED="1572728284362" LINK="https://www.w3schools.com/jquery/html_outerheight.asp" MODIFIED="1572728284362" TEXT="https://www.w3schools.com/jquery/html_outerheight.asp"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1572720823869" ID="ID_148083690" MODIFIED="1572720825510" POSITION="right" TEXT="apache">
+<node CREATED="1572720826367" ID="ID_1542310742" MODIFIED="1572720834370" TEXT="servername vs listen">
+<node CREATED="1572725263572" ID="ID_1521948107" MODIFIED="1572725289006" TEXT="conf&#xc5d0;&#xc11c; servename&#xc744; &#xc124;&#xc815;&#xd558;&#xc9c0; &#xc54a;&#xc73c;&#xba74; &#xc5d0;&#xb7ec;&#xac00; &#xb09c;&#xb2e4;."/>
+<node CREATED="1572725289282" ID="ID_1297426079" MODIFIED="1572725305235" TEXT="&#xadf8;&#xb798;&#xc11c; localshost:80&#xc73c;&#xb85c; &#xc124;&#xc815;&#xc744; &#xd588;&#xb294;&#xb370;"/>
+<node CREATED="1572725305561" ID="ID_1776082832" MODIFIED="1572725324695" TEXT="&#xc774;&#xac8c; listen &#xadf8;&#xb7ec;&#xb2c8;&#xae4c; &#xd3ec;&#xd2b8; &#xc124;&#xc815;&#xd560;&#xb54c; &#xbb38;&#xc81c;&#xac00; &#xb41c;&#xb2e4;."/>
+<node CREATED="1572725346278" ID="ID_402235370" MODIFIED="1572725384045" TEXT="listen &#xacfc; &#xb611;&#xac19;&#xc740; &#xd3ec;&#xd2b8;&#xb85c; &#xd558;&#xba74; &#xbcd1;&#xc2e0;&#xc774; &#xb41c;&#xb2e4;."/>
+<node CREATED="1572725384263" ID="ID_355148071" MODIFIED="1572725386515" TEXT="&#xc878;&#xb77c; &#xc6c3;&#xae34;&#xb2e4;"/>
+<node CREATED="1572725387318" ID="ID_36460181" MODIFIED="1572725398685" TEXT="&#xc5ec;&#xd558;&#xd2bc;&#xac04;&#xc5d0; servernme&#xc744; 800 &#xc73c;&#xb85c; listen&#xc744; 80&#xc73c;&#xb85c; &#xd558;&#xba74;&#xb41c;&#xb2e4;."/>
+</node>
+</node>
+</node>
+</map>
